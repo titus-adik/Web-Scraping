7a49f8dd3d443216c8a924885d565b633449d43c
Titus Adik <otienoadik@gmail.com>
2026-08-02 15:16:37 +0300

    Update scrape_scire_py.py

--- scrape_scire_py.py
+++ scrape_scire_py.py
@@ -174,32 +174,6 @@ if why_choose_us:
 
 
 
-"""# Web Scraping Project: Scire Research
-
-## Objective
-Scrape key information from https://scireresearch.com/
-
-## Data Collected
-- Company headline and description
-- Contact information (email, phone, location)
-- Service highlights and key paragraphs
-- "Why Choose Us" points
-
-## Tools Used
-- Python 3.x
-- requests library for HTTP requests
-- BeautifulSoup for HTML parsing
-- pandas for data manipulation
-- json module for structured storage
-
-## Output Files
-1. scire_research_data.json - Structured JSON data
-2. scire_research_data.csv - Flattened CSV data
-
-## How to Run
-python scrape_scire.py
-"""
-
 import json
 
 with open('scire_research_data.json', 'r') as f:
